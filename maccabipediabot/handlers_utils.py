4fc977441a960065304ad09b69eb77eb96bf9290
kosh-b <roeebaba@gmail.com>
2020-03-15 01:02:19 +0200

    Document telegram handlers decorators

--- maccabipediabot/handlers_utils.py
+++ maccabipediabot/handlers_utils.py
@@ -9,7 +9,11 @@ logger = logging.getLogger(__name__)
 @decorator
 def log_user_request(func, update, context, *args, **kwargs):
     """
-    Log the user name with the current function name
+    Logs the current activity of the user.
+    :param func: Function to decorate
+    :type func: callable
+    :type update: telegram.update.Update
+    :type context: telegram.ext.callbackcontext.CallbackContext
     """
     if update.callback_query:
         user_text = update.callback_query.data
@@ -24,6 +28,10 @@ def log_user_request(func, update, context, *args, **kwargs):
 def send_typing_action(func, update, context, *args, **kwargs):
     """
     Send "Typing..." to the user while processing the request
+    :param func: Function to decorate
+    :type func: callable
+    :type update: telegram.update.Update
+    :type context: telegram.ext.callbackcontext.CallbackContext
     """
     context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
     return func(update, context, *args, **kwargs)
