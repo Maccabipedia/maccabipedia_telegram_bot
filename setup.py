3fa801c91fba94f17545281696d4daa1f29f893e
kosh-b <roeebaba@gmail.com>
2020-03-15 02:27:43 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(
     python_requires='>=3',
     install_requires=["python-telegram-bot==12.4.2",
                       "decorator==4.4.2",
-                      "maccabistats==2.9.1",
-                      "python-dotenv-0.12.0"]
+                      "maccabistats==2.9.2",
+                      "python-dotenv=0.12.0"]
 )
