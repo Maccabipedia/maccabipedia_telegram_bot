197723724e72f84a1977b9469a749d2adb5c99c5
kosh-b <roeebaba@gmail.com>
2020-04-04 02:21:43 +0300

    Recommend similar songs names

--- maccabipediabot/simple_flows/song_details_flow.py
+++ maccabipediabot/simple_flows/song_details_flow.py
@@ -4,6 +4,7 @@ from telegram.ext import ConversationHandler, CommandHandler, MessageHandler, Fi
 from maccabipediabot.common import get_song_lyrics
 from maccabipediabot.handlers_utils import log_user_request, send_typing_action, go_back_to_main_menu_from_conversation_handler
 from maccabipediabot.main_user_keyboard import MainKeyboardOptions, create_main_user_reply_keyboard, create_go_back_reply_keyboard
+from maccabipediabot.similar_song_matcher import song_exists_on_maccabipedia, SimilarMaccabiPediaSong
 
 _show_song_details_state = range(1)
 
@@ -15,11 +16,23 @@ def show_song_details_handler(update, context):
     :return: Lyrics of the given song & link to the page
     """
     song_name = update.message.text
-    # TODO: We might want to stay in the same state if we could not find the song (so the user wont need to press on the keyboard again)
-    context.bot.send_message(chat_id=update.effective_chat.id, parse_mode=ParseMode.HTML, text=get_song_lyrics(song_name),
-                             reply_markup=create_main_user_reply_keyboard())
 
-    return ConversationHandler.END
+    if not song_exists_on_maccabipedia(song_name):
+        similar_songs_pages_names = SimilarMaccabiPediaSong(song_name).find_most_similar_songs()
+        if similar_songs_pages_names:
+            pretty_print_of_similar_songs_pages = "\n".join(page_name for page_name in similar_songs_pages_names)
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f"לא נמצא שיר בשם: '{song_name}', אלו השירים עם השם הדומה ביותר:"
+                                                                            f"\n{pretty_print_of_similar_songs_pages}"
+                                                                            f"\n\nשלח את שם השיר הרצוי")
+        else:
+            context.bot.send_message(chat_id=update.effective_chat.id, text=f"לא נמצא שיר בשם: '{song_name}' נסה בשנית")
+
+        return _show_song_details_state
+    else:  # This song exists on maccabipedia, lets show it!
+        context.bot.send_message(chat_id=update.effective_chat.id, parse_mode=ParseMode.HTML, text=get_song_lyrics(song_name),
+                                 reply_markup=create_main_user_reply_keyboard())
+
+        return ConversationHandler.END
 
 
 @log_user_request
